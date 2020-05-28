d107898a901643cd4abead7ec3a102243ceccba6
blademainer <blademainer@gmail.com>
2020-05-28 21:26:05 +0800

    update printstack

--- pkg/recover/panic.go
+++ pkg/recover/panic.go
@@ -2,21 +2,21 @@ package recover
 
 import (
 	"fmt"
-	"runtime"
+	"runtime/debug"
 )
 
 type PanicHandle func(interface{})
 
-func PrintStack() {
-	var buf [4096]byte
-	n := runtime.Stack(buf[:], false)
-	fmt.Printf("==> %s\n", string(buf[:n]))
-}
+//func PrintStack() {
+//	var buf [4096]byte
+//	n := runtime.Stack(buf[:], false)
+//	fmt.Printf("==> %s\n", string(buf[:n]))
+//}
 
 func Recover() {
 	if err := recover(); err != nil {
 		fmt.Println(err) // 这里的err其实就是panic传入的内容，55
-		PrintStack()
+		debug.PrintStack()
 	}
 }
 
